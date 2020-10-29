fdbb97af39a7ec311930b04a01f65a7615a4e042
Dhrumil <dhrumilmalaviya@hotmail.com>
2020-10-29 00:31:11 -0400

    added swipe features

---
+++ ShoppingApplication/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  ShoppingApplication
+//
+//  Created by Dhrumil Malaviya on 2020-10-28.
+//  Copyright © 2020 Dhrumil Malaviya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController
+{
+
+    @IBOutlet weak var labelFav1: UILabel!
+    @IBOutlet weak var lableFav2: UILabel!
+    @IBOutlet weak var labelFav3: UILabel!
+    @IBOutlet weak var labelFav4: UILabel!
+    @IBOutlet weak var labelFav5: UILabel!
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+    
+
+}
--- ShoppingApplication/ViewController.swift
+++ ShoppingApplication/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+var flag=0
 class ViewController: UIViewController , UITextFieldDelegate
 {
 
@@ -28,6 +28,11 @@ class ViewController: UIViewController , UITextFieldDelegate
     @IBOutlet weak var scrollView: UIScrollView!
     
     
+    @IBOutlet weak var stepper1: UIStepper!
+    @IBOutlet weak var stepper2: UIStepper!
+    @IBOutlet weak var stepper3: UIStepper!
+    @IBOutlet weak var stepper4: UIStepper!
+    @IBOutlet weak var stepper5: UIStepper!
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -37,17 +42,66 @@ class ViewController: UIViewController , UITextFieldDelegate
         self.textInputItem3.delegate = self
         self.textInputItem4.delegate = self
         self.textInputItem5.delegate = self
+        
+        
+         // MARK: - Swipe Methods
+        let swipeRight1 = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestureForInputText1))
+        swipeRight1.direction = UISwipeGestureRecognizer.Direction.right
+        textInputItem1.addGestureRecognizer(swipeRight1)
+        
+        let swipeLeft1 = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestureForInputText1))
+        swipeLeft1.direction = UISwipeGestureRecognizer.Direction.left
+        textInputItem1.addGestureRecognizer(swipeLeft1)
+        
+        let swipeRight2 = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestureForInputText2))
+               swipeRight2.direction = UISwipeGestureRecognizer.Direction.right
+               textInputItem2.addGestureRecognizer(swipeRight2)
+               
+               let swipeLeft2 = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestureForInputText2))
+               swipeLeft2.direction = UISwipeGestureRecognizer.Direction.left
+               textInputItem2.addGestureRecognizer(swipeLeft2)
+        
+        let swipeRight3 = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestureForInputText3))
+               swipeRight3.direction = UISwipeGestureRecognizer.Direction.right
+               textInputItem3.addGestureRecognizer(swipeRight3)
+               
+               let swipeLeft3 = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestureForInputText3))
+               swipeLeft3.direction = UISwipeGestureRecognizer.Direction.left
+               textInputItem3.addGestureRecognizer(swipeLeft3)
+        
+        let swipeRight4 = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestureForInputText4))
+               swipeRight4.direction = UISwipeGestureRecognizer.Direction.right
+               textInputItem4.addGestureRecognizer(swipeRight4)
+               
+               let swipeLeft4 = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestureForInputText4))
+               swipeLeft4.direction = UISwipeGestureRecognizer.Direction.left
+               textInputItem4.addGestureRecognizer(swipeLeft4)
+        
+        let swipeRight5 = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestureForInputText5))
+               swipeRight5.direction = UISwipeGestureRecognizer.Direction.right
+               textInputItem5.addGestureRecognizer(swipeRight5)
+               
+               let swipeLeft5 = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestureForInputText5))
+               swipeLeft5.direction = UISwipeGestureRecognizer.Direction.left
+               textInputItem5.addGestureRecognizer(swipeLeft5)
+        
     }
     
     
+     // MARK: -  Fuctions for delegate method, that is used to validate text that was entered by the user
     
     func textFieldDidBeginEditing(_ textField: UITextField)
         {
-             scrollView.setContentOffset(CGPoint(x: 0,y: 400), animated: true)
-        }
+            if(textField==textInputItem4 || textField==textInputItem5)
+            {
+             scrollView.setContentOffset(CGPoint(x: 0,y: 10), animated: true)      //setting up a scrollview so that textinput4 and texxtinput 5 are not hidden behind the keyboard
+            }
+            }
     func textFieldDidEndEditing(_ textField: UITextField)
         {
-            scrollView.setContentOffset(CGPoint(x: 0,y: 0), animated: true)
+             if(textField==textInputItem4 || textField==textInputItem5)
+             {
+                scrollView.setContentOffset(CGPoint(x: 0,y: -50), animated: true)}
         }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool //dismiss the keyboard when user presses return button
        {
@@ -55,31 +109,156 @@ class ViewController: UIViewController , UITextFieldDelegate
             return true
        }
 
-    
+     // MARK: - Action methods for stepper
     @IBAction func stepper1(_ sender: UIStepper)
     {
+        
         labelStepper1.text=String(format: "%.0f", sender.value)
     }
     
     @IBAction func stepper2(_ sender: UIStepper)
     {
+        
        labelStepper2.text=String(format: "%.0f", sender.value)
     }
     
     @IBAction func stepper3(_ sender: UIStepper)
     {
+       
         labelStepper3.text=String(format: "%.0f", sender.value)
     }
     
     @IBAction func stepper4(_ sender: UIStepper)
     {
+       
         labelStepper4.text=String(format: "%.0f", sender.value)
     }
     
     @IBAction func stepper5(_ sender: UIStepper)
     {
+       
         labelStepper5.text=String(format: "%.0f", sender.value)
     }
     
+     // MARK: - Action methods for Buttons
+    @IBAction func onCancelledButtonPressed(_ sender: UIButton)
+    {
+        //Using alert to confirm from user weather they want to delete entries or not
+        let alert = UIAlertController(title: "Cancel", message: "Are you sure you want to clear all entries", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: {action in ()} ))
+        //addAction method is used to provide addtional buttons in the alert box. Style "cancel" will cancel the alert box
+         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {action in
+            //Style "default" will perform the default operations in the handler
+           
+            self.reset()
+            
+            
+         } ))
+        present(alert,animated: true)  //present is used to generate the pop up window in the user screen
+    }
+    
+// MARK: - SWIPE METHODS
+    
+    @IBAction func swipeActionForTextInput1(_ sender: Any)
+    {
+        
+    }
+    
+    @objc func swipeGestureForInputText1 (sender: UISwipeGestureRecognizer?)
+    {
+        if let swipeGestureForInputText1 = sender
+        {
+            switch swipeGestureForInputText1.direction
+            {
+            case UISwipeGestureRecognizer.Direction.right :
+                textInputItem1.text="Right Swipe"
+            case UISwipeGestureRecognizer.Direction.left :
+                textInputItem1.text=""
+            default:
+                break
+            }
+        }
+    }
+    
+    
+    
+    @objc func swipeGestureForInputText2 (sender: UISwipeGestureRecognizer?)
+       {
+           if let swipeGestureForInputText2 = sender
+           {
+               switch swipeGestureForInputText2.direction
+               {
+               case UISwipeGestureRecognizer.Direction.right :
+                   textInputItem2.text="Right Swipe"
+               case UISwipeGestureRecognizer.Direction.left :
+                   textInputItem2.text=""
+               default:
+                   break
+               }
+           }
+       }
+    @objc func swipeGestureForInputText3 (sender: UISwipeGestureRecognizer?)
+       {
+           if let swipeGestureForInputText3 = sender
+           {
+               switch swipeGestureForInputText3.direction
+               {
+               case UISwipeGestureRecognizer.Direction.right :
+                   textInputItem3.text="Right Swipe"
+               case UISwipeGestureRecognizer.Direction.left :
+                   textInputItem3.text=""
+               default:
+                   break
+               }
+           }
+       }
+    @objc func swipeGestureForInputText4 (sender: UISwipeGestureRecognizer?)
+       {
+           if let swipeGestureForInputText4 = sender
+           {
+               switch swipeGestureForInputText4.direction
+               {
+               case UISwipeGestureRecognizer.Direction.right :
+                   textInputItem4.text="Right Swipe"
+               case UISwipeGestureRecognizer.Direction.left :
+                   textInputItem4.text=""
+               default:
+                   break
+               }
+           }
+       }
+    @objc func swipeGestureForInputText5 (sender: UISwipeGestureRecognizer?)
+       {
+           if let swipeGestureForInputText5 = sender
+           {
+               switch swipeGestureForInputText5.direction
+               {
+               case UISwipeGestureRecognizer.Direction.right :
+                   textInputItem5.text="Right Swipe"
+               case UISwipeGestureRecognizer.Direction.left :
+                   textInputItem5.text=""
+               default:
+                   break
+               }
+           }
+       }
+    func reset()
+    {
+        textInputItem1.text=""
+        textInputItem2.text=""
+        textInputItem3.text=""
+        textInputItem4.text=""
+        textInputItem5.text=""
+        labelStepper1.text="0"
+        labelStepper2.text="0"
+        labelStepper3.text="0"
+        labelStepper4.text="0"
+        labelStepper5.text="0"
+        stepper1.value=0
+        stepper2.value=0
+        stepper3.value=0
+        stepper4.value=0
+        stepper5.value=0
+    }
 }
 
